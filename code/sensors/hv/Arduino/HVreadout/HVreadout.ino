73a52e6b3e2fb7602d6314962b26b9118dd60422
Anna Pollmann <anna@pollmann.co>
2025-06-02 12:33:04 +0900

    New version with 2 HV

--- code/sensors/hv/Arduino/HVreadout/HVreadout.ino
+++ code/sensors/hv/Arduino/HVreadout/HVreadout.ino
@@ -1,20 +1,20 @@
+/*
+This script can drive 2 HV devices right now
+- provides ON switch for HV on digital pin
+- provides HV read # TODO switch to picoscope?
+*/
+
 // PIN setup
-int VmonPIN = A0; // Vmon
-int EnablePIN = 2; // Enable pin on digital pin 2
+int VmonPIN1 = A0; // Vmon
+int VmonPIN2 = A6; // Vmon
+int EnablePIN1 = 2; // Enable pin on digital pin 2
+int EnablePIN2 = 3; // Enable pin on digital pin 2
 
-// variables
-int VmonValue = 0; // Variable für den Sensorwert mit 0 als Startwert
-float Vmon = 0; // voltage value of Vmon
-float hv = 0; // converted hv value of Vmon
-String incomingMSG = ""; // gets incoming serial communication
-float accuracy = 0; // calculate accuracy of hv
-
-// constants
-// constants
-float monCoeff = 607.9; // measured
-float monOffset=-28.9;
-//float setCoeff = 623.7; // measured 
-//float setOffset=1.5
+// constants for voltage conversion
+float monCoeff1 = 607.9; // measured
+float monOffset1=-28.9;
+float monCoeff2 = monCoeff1; // measured
+float monOffset2=-monOffset2;
 
 float hvAcc = 0.005; // from datasheet
 float arduinoAcc = 5000.0/1023.0 / 1000.0 ; // in V
@@ -23,18 +23,33 @@ float arduinoAcc = 5000.0/1023.0 / 1000.0 ; // in V
 void setup(){
   Serial.begin(9600);
   
-  pinMode(EnablePIN, OUTPUT);
-  digitalWrite(EnablePIN, HIGH); // New: ensure being online, could also be used to switch on/off manually
+  pinMode(EnablePIN1, OUTPUT);
+  digitalWrite(EnablePIN1, HIGH); // New: ensure being online, could also be used to switch on/off manually
+
+  pinMode(EnablePIN2, OUTPUT);
+  digitalWrite(EnablePIN2, HIGH); // New: ensure being online, could also be used to switch on/off manually
 
   delay(500);
 }
 
-void sendValue(){
+void sendValue(int devicenumber){
+
+    if (devicenumber == 1){
+      VmonPIN=VmonPIN1;
+      monCoeff=monCoeff1;
+      monOffset=monOffset1;
+    }
+    else {
+      VmonPIN=VmonPIN2;
+      monCoeff=monCoeff2;
+      monOffset=monOffset2;
+    }
+
     // read Vmon
-    VmonValue =analogRead(VmonPIN); //
-    Vmon = VmonValue * (5000.0 / 1023.0); // 10 bit // -> mV
-    hv =   Vmon * monCoeff / 1000.0 + monOffset ; // -> V
-    accuracy = sqrt( sq(arduinoAcc * monCoeff) + sq(hvAcc*hv) ); // add in quadrature because uncertainties are not correlated, arduinoCoeff translated to HV before this
+    int VmonValue =analogRead(VmonPIN); //
+    float Vmon = VmonValue * (5000.0 / 1023.0); // 10 bit // -> mV
+    float hv =   Vmon * monCoeff / 1000.0 + monOffset ; // -> V
+    float accuracy = sqrt( sq(arduinoAcc * monCoeff) + sq(hvAcc*hv) ); // add in quadrature because uncertainties are not correlated, arduinoCoeff translated to HV before this
     Serial.print("\tVmon "); // do not change this output! otherwise python receiver will break
     Serial.print(Vmon);
     Serial.print(" mV; HV "); 
@@ -54,7 +69,10 @@ void loop(){
       digitalWrite(LED_BUILTIN, HIGH);
       int val = char(Serial.read()) - '0';
       if (val == 1){
-        sendValue();
+        sendValue(1);
+      }
+      else if (val == 2){
+        sendValue(2);
       }
       delay(200);
   }  
